a74230eb70edf34908f95658f6467135bf8ee143
Fazlay rabbi <rfazlay21@gmail.com>
2022-02-17 00:21:43 +0600

    test successsful

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,33 +9,72 @@ import {
   View,
   Button,
   TextInput,
-} from "react-native";
-import SinglelineTextInputNativeComponent from "react-native/Libraries/Components/TextInput/RCTMultilineTextInputNativeComponent";
+} from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState("Useless Text");
-
-  console.log(text);
+  const [text, setText] = useState('Useless Text');
 
   const handleSubmit = () => {
-    console.log(text);
+    // emailjs.init('user_bYgWfFHFkKyvzGnEDagVD');
+    // const data = {
+    //   service_id: 'service_72716sm',
+    //   template_id: 'template_f7efh85',
+    //   template_params: {},
+    //   user_id: 'user_bYgWfFHFkKyvzGnEDagVD',
+    // };
+
+    // fetch('https://api.emailjs.com/api/v1.0/email/send', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   data: JSON.stringify(data),
+    // })
+    //   .then((res) => res.text())
+    //   .then((json) => console.log(json))
+    //   .catch((err) => console.log(err));
+
+    // emailjs.send('service_72716sm', 'template_9q258ou', 'fazlay').then(
+    //   (result) => {
+    //     console.log(result.text);
+    //   },
+    //   (error) => {
+    //     console.log(error.text);
+    //   }
+    // );.
+    //user_id :user_bYgWfFHFkKyvzGnEDagVD
+    emailjs
+      .send(
+        'service_72716sm',
+        'template_f18li4r',
+        { message: text },
+        'user_bYgWfFHFkKyvzGnEDagVD'
+      )
+      .then(
+        function (response) {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        function (error) {
+          console.log('FAILED...', error);
+        }
+      );
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ flexDirection: "column", marginTop: 100, padding: 10 }}>
+      <View style={{ flexDirection: 'column', marginTop: 100, padding: 10 }}>
         <Text>Open up App.js to start working on your app!</Text>
         <TextInput onChangeText={(text) => setText(text)} value={text} />
 
-        <ExpoStatusBar style="auto" />
+        <ExpoStatusBar style='auto' />
 
         <Button
           onPress={() => {
             handleSubmit();
           }}
-          title="Learn More"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
+          title='Learn More'
+          color='#841584'
+          accessibilityLabel='Learn more about this purple button'
         />
       </View>
     </SafeAreaView>
@@ -45,6 +85,6 @@ const styles = StyleSheet.create({
   container: {
     marginTop: StatusBar.currentHeight,
 
-    backgroundColor: "#ffff",
+    backgroundColor: '#ffff',
   },
 });
